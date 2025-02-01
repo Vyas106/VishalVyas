4811bfc9f0474694ca9758acee53f0058fd486a5
vyasvishal <vyasvishal106.work@gmail.com>
2025-02-01 21:33:31 +0530

    commit

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,9 +1,9 @@
 "use client"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
+// import type { ThemeProviderProps } from "next-themes"
 import { useState, useEffect } from "react"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: any) {
   const [mounted, setMounted] = useState(false)
 
   useEffect(() => {
