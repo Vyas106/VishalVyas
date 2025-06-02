3a1077ff1c9bcf91d4eb9323c1da8e614d98ff73
vyasvishal <vyasvishal106.work@gmail.com>
2025-06-02 11:09:13 +0530

    first commit 2

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: [
+      "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+      "res.cloudinary.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
